42660b4a81b825fc8fd727158f77ebd450a47336
matheuwr8 <matheusswr8@gmail.com>
2025-05-26 22:44:49 -0300

    Método para criação do dispositivo no fiware

--- PBL/Controllers/ClienteFiware.cs
+++ PBL/Controllers/ClienteFiware.cs
@@ -9,11 +9,11 @@ namespace PBL.Controllers
 {
     public static class ClienteFiware
     {
-        public static async Task<string> Get(string url)
+        public static async Task<string> Get(HttpRequestMessage request)
         {
             using (var httpClient = new HttpClient())
             {
-                var response = await httpClient.GetAsync(url);
+                var response = await httpClient.SendAsync(request);
                 if (response.IsSuccessStatusCode)
                 {
                     return await response.Content.ReadAsStringAsync();
@@ -24,60 +24,5 @@ namespace PBL.Controllers
                 }
             }
         }
-
-
-        public static async Task<string> Post(string url, string jsonBody)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
-                var response = await httpClient.PostAsync(url, content);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    return await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    throw new Exception($"Erro na requisição (Post). Code: {response.StatusCode}");
-                }
-            }
-        }
-
-        public static async Task<string> Put(string url, string jsonBody)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
-                var response = await httpClient.PutAsync(url, content);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    return await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    throw new Exception($"Erro na requisição (Put). Code: {response.StatusCode}");
-                }
-            }
-        }
-
-        public static async Task<string> DeleteAsync(string url)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                var response = await httpClient.DeleteAsync(url);
-                if (response.IsSuccessStatusCode)
-                {
-                    return await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    throw new Exception($"Erro na requisição (Delete). Code: {response.StatusCode}");
-                }
-            }
-        }
-
-
     }
 }
--- PBL/Controllers/DispositivoController.cs
+++ PBL/Controllers/DispositivoController.cs
@@ -4,6 +4,7 @@ using PBL.DAO;
 using PBL.Models;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 
 namespace PBL.Controllers
 {
@@ -24,5 +25,36 @@ namespace PBL.Controllers
             if (string.IsNullOrEmpty(model.EntityName))
                 ModelState.AddModelError("EntityName", "Digite um nome para o Fiware");
         }
+
+        private void CriarDispositivoFiware(DispositivoViewModel model) {
+            var request = new HttpRequestMessage(HttpMethod.Post, "http://3.90.209.194:4041/iot/devices");
+            request.Headers.Add("fiware-service", "smart");
+            request.Headers.Add("fiware-servicepath", "/");
+            var content = new StringContent("{\"devices\": [{" +
+                                            $"      \"device_id\": \"{model.DeviceId}\",         " +
+                                            "      \"entity_name\": \"urn:ngsi-ld:DataLogger:001\",   " +
+                                            "      \"entity_type\": \"DataLogger\",          " +
+                                            "      \"protocol\": \"PDI-IoTA-UltraLight\",  " +
+                                            "      \"transport\": \"MQTT\",            " +
+                                            "" +
+                                            "      " +
+                                            "      \"commands\": [" +
+                                            "        { \"name\": \"on\", \"type\": \"command\" },  " +
+                                            "        { \"name\": \"off\", \"type\": \"command\" }  " +
+                                            "      ]," +
+                                            "" +
+                                            "      " +
+                                            "      \"attributes\": [" +
+                                            "        { \"object_id\": \"s\", \"name\": \"state\", \"type\": \"Text\" }, " +
+                                            "        { \"object_id\": \"v\", \"name\": \"voltage\", \"type\": \"Float\"}," +
+                                            "        { \"object_id\": \"t\", \"name\": \"temperature\", \"type\": \"Float\"}" +
+                                            "      ]" +
+                                            "    }" +
+                                            "  ]" +
+                                            "}", null, "application/json");
+            request.Content = content;
+            ClienteFiware.Get(request);
+        }
+
     }
 }
